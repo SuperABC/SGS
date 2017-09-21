0669439d8214fc9652bddcc820ad9c118937d2aa
SG master <1363028533@qq.com>
2017-09-21 13:59:17 +0800

    Add files via upload.

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "machine.h"
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main() {
+	Lexeme l = Lexeme();
+	Syntax s = Syntax();
+	Machine m = Machine();
+
+	string input;
+
+	while (1) {
+		getline(cin, input);
+		try {
+			l.input(input.c_str())->parse();
+			s.input(l.strId, l.output)->parse();
+			m.input(s.output, s.globeClassType, s.globeFunc, s.globeVar)->execute();
+		}
+		catch (LexemeException *le) {
+			cout << le->message();
+		}
+		catch (SyntaxException *se) {
+			cout << se->message();
+		}
+	}
+}
