717d5aa3e43ee2fb4e661fdefd96fad420470535
SG master <1363028533@qq.com>
2018-02-25 17:47:19 +0800

    Update.

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <fstream>
 #include "machine.h"
 
-#define ENTER_OMIT
+//#define ENTER_OMIT
 
 using std::cin;
 using std::cout;
@@ -18,9 +18,57 @@ int main() {
 	std::ifstream fin("test.sgs");
 
 #ifndef  ENTER_OMIT 
+	string next = string("$");
 	while (1) {
-		if (fin.eof())break;
-		getline(fin, input);
+		if (fin.eof()) {
+			input = next;
+			break;
+		}
+
+		if (next == string("$")) {
+			getline(fin, input);
+			if (fin.eof())break;
+			getline(fin, next);
+			if (next[0] == ' ' || next[0] == '\t') {
+				while (next[0] == ' ' || next[0] == '\t') {
+					input += next;
+					if (fin.eof())Syntax::error("", SE_EXPOSE);
+					getline(fin, next);
+				}
+				input += next;
+				if (fin.eof())break;
+				getline(fin, next);
+			}
+		}
+		else {
+			input = next;
+			getline(fin, next);
+			if (next[0] == ' ' || next[0] == '\t') {
+				while (next[0] == ' ' || next[0] == '\t') {
+					input += next;
+					if (fin.eof())Syntax::error("", SE_EXPOSE);
+					getline(fin, next);
+				}
+				input += next;
+				if (fin.eof())break;
+				getline(fin, next);
+			}
+		}
+		try {
+			l.input(input.c_str())->parse();
+			s.input(l.strId, l.output)->parse();
+			m.input(s.output, s.globeClassType, s.globeFunc, s.globeVar)->execute();
+		}
+		catch (LexemeException *le) {
+			cout << l.get() << endl;
+			cout << le->message() << endl;
+		}
+		catch (SyntaxException *se) {
+			cout << l.get() << endl;
+			cout << se->message() << endl;
+		}
+	}
+	if (input != string("$")) {
 		try {
 			l.input(input.c_str())->parse();
 			s.input(l.strId, l.output)->parse();
