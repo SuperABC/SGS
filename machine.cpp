f9c2a13f5ca5c3ed21bf167d5309e76142837cfe
SG master <1363028533@qq.com>
2018-02-26 19:50:50 +0800

    Update.

--- machine.cpp
+++ machine.cpp
@@ -1,5 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include "machine.h"
+#include <time.h>
 
 Machine::Machine() {
 
@@ -9,8 +10,8 @@ Machine::~Machine() {
 }
 
 Machine *Machine::input(stateSeq *seq,
-	vector<classType> classType, vector<funcNode> func,
-	vector<varNode> var) {
+	vector<classType> &classType, vector<funcNode> &func,
+	vector<varNode> &var) {
 	content = seq;
 	globeClassType = classType;
 	globeFunc = func;
@@ -225,8 +226,16 @@ void Machine::step(stateSeq *s) {
 					}
 
 					break;
+				case VT_CLASS:
+					if (tmp.t == VT_CLASS && tmp.classType == s->act.left->classType)
+						globeVar[i].val = tmp.val;
+					else
+						error(s->act.left->classType.c_str(), VE_TYPEMISMATCH);
+					break;
 				case VT_NULL:
-					globeVar[i] = tmp;
+					globeVar[i].t = tmp.t;
+					globeVar[i].val = tmp.val;
+					globeVar[i].classType = tmp.classType;
 
 					break;
 				}
@@ -321,7 +330,7 @@ void Machine::step(stateSeq *s) {
 								error(s->act.left->name.c_str(), VE_TYPEMISMATCH);
 						}
 						else {
-							varNode *start = (varNode *)globeVar[i].val;
+							varNode *start = (varNode *)globeFunc[func].localVar[i].val;
 							while (offset--) {
 								start = start->next;
 							}
@@ -404,9 +413,16 @@ void Machine::step(stateSeq *s) {
 						}
 
 						break;
+					case VT_CLASS:
+						if (tmp.t == VT_CLASS && tmp.classType == s->act.left->classType)
+							globeFunc[func].localVar[i].val = tmp.val;
+						else
+							error(s->act.left->classType.c_str(), VE_TYPEMISMATCH);
+						break;
 					case VT_NULL:
 						globeFunc[func].localVar[i].t = tmp.t;
 						globeFunc[func].localVar[i].val = tmp.val;
+						globeFunc[func].localVar[i].classType = tmp.classType;
 
 						break;
 					}
@@ -421,11 +437,10 @@ void Machine::step(stateSeq *s) {
 		varNode *par = new varNode();
 		varNode *start = par;
 		while (iter != NULL) {
-			if (iter->t == VT_VAR || iter->t == VT_EXP || iter->t == VT_FUNCTION) {
-				*par = exp(iter);
-			}
+			*par = exp(iter);
 			par->name = iter->name;
 			iter = iter->next;
+			if(iter)par->next = new varNode();
 			par = par->next;
 		}
 		if (s->act.left->name == "out") {
@@ -433,6 +448,12 @@ void Machine::step(stateSeq *s) {
 		}
 		else if (s->act.left->name == "outln") {
 			outln(start);
+		}
+		else if (s->act.left->name == "current time") {
+			time(start);
+		}
+		else if(s->act.left->name == "in") {
+			in(start);
 		}
 		else {
 			for (unsigned int i = 0; i < globeFunc.size(); i++) {
@@ -481,20 +502,31 @@ void Machine::step(stateSeq *s) {
 		}
 	}
 }
-
 varNode Machine::exp(varNode *e) {
 	varNode ret;
 	if (e->t == VT_FUNCTION) {
-		for (unsigned int i = 0; i < globeFunc.size(); i++) {
-			if (globeFunc[i].declare.name == e->block->act.left->name) {
-				execute(&globeFunc[i].content, e->block->act.right, i);
-				ret = globeFunc[i].localVar[0];
+		if (e->block->act.left->name == "current time") {
+			ret = time(e->block->act.right);
+		}
+		else if (e->block->act.left->name == "in") {
+			ret = in(e->block->act.right);
+		}
+		else if (e->block->act.left->name == "random number") {
+			ret = random(e->block->act.right);
+		}
+		else {
+			for (unsigned int i = 0; i < globeFunc.size(); i++) {
+				if (globeFunc[i].declare.name == e->block->act.left->name) {
+					execute(&globeFunc[i].content, e->block->act.right, i);
+					ret = globeFunc[i].localVar[0];
+				}
 			}
 		}
 	}
 	else if (e->t == VT_EXP) {
 		varNode l = exp(e->left);
 		varNode r = exp(e->right);
+
 		switch (*(int *)e->val) {
 		case OP_PLUS:
 			if (l.t == VT_FLOAT && r.t == VT_FLOAT) {
@@ -559,18 +591,26 @@ varNode Machine::exp(varNode *e) {
 		case OP_DIVIDE:
 			if (l.t == VT_FLOAT && r.t == VT_FLOAT) {
 				ret.t = VT_FLOAT;
+				if (*(float *)(l.val) == 0)
+					error("", VE_DIVBYZERO);
 				ret.val = new float(*(float *)(r.val) / *(float *)(l.val));
 			}
 			else if (l.t == VT_FLOAT && r.t == VT_INTEGER) {
 				ret.t = VT_FLOAT;
+				if (*(float *)(l.val) == 0)
+					error("", VE_DIVBYZERO);
 				ret.val = new float(*(int *)(r.val) / *(float *)(l.val));
 			}
 			else if (l.t == VT_INTEGER && r.t == VT_FLOAT) {
 				ret.t = VT_FLOAT;
+				if (*(int *)(l.val) == 0)
+					error("", VE_DIVBYZERO);
 				ret.val = new float(*(float *)(r.val) / *(int *)(l.val));
 			}
 			else if (l.t == VT_INTEGER && r.t == VT_INTEGER) {
 				ret.t = VT_INTEGER;
+				if (*(int *)(l.val) == 0)
+					error("", VE_DIVBYZERO);
 				ret.val = new int(*(int *)(r.val) / *(int *)(l.val));
 			}
 			break;
@@ -711,73 +751,131 @@ varNode Machine::exp(varNode *e) {
 	}
 	else {
 		ret.t = e->t;
+		ret.classType = e->classType;
 		ret.val = e->val;
 		ret.next = e->next;
 	}
 
 	return ret;
 }
+
+void Machine::print(varNode *par) {
+	switch (par->t) {
+	case VT_STRING:
+		printf("%s", (char *)par->val);
+		break;
+	case VT_INTEGER:
+		printf("%d", *(int *)(par->val));
+		break;
+	case VT_FLOAT:
+		printf("%f", *(float *)(par->val));
+		break;
+	case VT_CHAR:
+		printf("%c", *(char *)(par->val));
+		break;
+	case VT_BOOL:
+		if(*(bool *)(par->val))
+			printf("true");
+		else printf("false");
+		break;
+	case VT_VAR:
+		print(&exp(par));
+		break;
+	case VT_ARRAY: {
+		varNode * iter = (varNode *)par->val;
+		while (iter) {
+			print(iter);
+			printf(" ");
+			iter = iter->next;
+		}
+		break;
+	}
+	case VT_CLASS: {
+		classNode * tmp = (classNode *)par->val;
+		printf("{ ");
+		for (auto &iter : tmp->varList) {
+			print(&iter);
+			printf(" ");
+		}
+		printf("}");
+		break;
+	}
+	}
+}
 void Machine::out(varNode *par) {
 	if (par->name == "content") {
-		if(par->t==VT_STRING)
-			printf("%s", (char *)par->val);
-		if(par->t==VT_INTEGER)
-			printf("%d", *(int *)(par->val));
-		if (par->t == VT_FLOAT)
-			printf("%f", *(float *)(par->val));
-		if (par->t == VT_ARRAY) {
-			varNode *iter = (varNode *)par->val;
-			while (iter) {
-				switch (iter->t) {
-				case VT_STRING:
-					printf("%s ", (char *)iter->val);
-					break;
-				case VT_INTEGER:
-					printf("%d ", *(int *)(iter->val));
-					break;
-				case VT_FLOAT:
-					printf("%f ", *(float *)(iter->val));
-					break;
-				}
-				iter = iter->next;
-			}
-		}
+		print(par);
 	}
 }
 void Machine::outln(varNode *par) {
 	if (par->name == "content") {
-		if (par->t == VT_STRING)
-			printf("%s\n", (char *)par->val);
-		if (par->t == VT_INTEGER)
-			printf("%d\n", *(int *)(par->val));
-		if (par->t == VT_FLOAT)
-			printf("%f\n", *(float *)(par->val));
-		if (par->t == VT_ARRAY) {
-			varNode *iter = (varNode *)par->val;
-			while (iter) {
-				switch (iter->t) {
-				case VT_STRING:
-					printf("%s ", (char *)iter->val);
-					break;
-				case VT_INTEGER:
-					printf("%d ", *(int *)(iter->val));
-					break;
-				case VT_FLOAT:
-					printf("%f ", *(float *)(iter->val));
-					break;
+		print(par);
+		printf("\n");
+	}
+}
+varNode Machine::in(varNode *par) {
+	for (auto &f : globeFunc) {
+		if (f.declare.name == "in") {
+			string *tmp = new string();
+			std::cin >> *tmp;
+
+			if ((*tmp)[0] >= '0'&&(*tmp)[0] <= '9') {
+				if (tmp->find('.') == -1) {
+					f.localVar[0].t = VT_INTEGER;
+					f.localVar[0].val = new int(atoi(tmp->c_str()));
+					return f.localVar[0];
 				}
-				iter = iter->next;
+				else {
+					f.localVar[0].t = VT_FLOAT;
+					f.localVar[0].val = new float((float)atof(tmp->c_str()));
+					return f.localVar[0];
+				}
 			}
-			printf("\n");
+			else {
+				f.localVar[0].t = VT_STRING;
+				f.localVar[0].val = tmp->c_str();
+				return f.localVar[0];
+			}
+
 		}
 	}
+	error("", VE_BROKEN);
+	return varNode();
+}
+varNode Machine::time(varNode *par) {
+	for (auto &f : globeFunc) {
+		if (f.declare.name == "current time") {
+			f.localVar[0].t = VT_INTEGER;
+			f.localVar[0].val = new int(clock());
+			return f.localVar[0];
+		}
+	}
+	error("", VE_BROKEN);
+	return varNode();
+}
+varNode Machine::random(varNode *par) {
+	for (auto &f : globeFunc) {
+		if (f.declare.name == "random number") {
+			f.localVar[0].t = VT_INTEGER;
+			f.localVar[0].val = new int(rand());
+			return f.localVar[0];
+		}
+	}
+	error("", VE_BROKEN);
+	return varNode();
+}
+
+void Machine::clearMem() {
+	macMem.clear();
 }
 
 void Machine::error(const char *inst, int type) {
 	switch (type) {
 	case VE_DIVBYZERO:
-		throw new SyntaxException( "0不能做被除数。");
+		throw new MachineException(std::string("0不能做被除数。"));
 	case VE_TYPEMISMATCH:
-		throw new SyntaxException(std::string(inst) + "类型不匹配。");
+		throw new MachineException(std::string(inst) + "类型不匹配。");
+	case VE_BROKEN:
+		throw new MachineException("虚拟机未准备或者已被破坏。");
 	}
 }
