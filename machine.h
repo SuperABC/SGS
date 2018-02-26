b355126fa4670b9d6aa197c4f01f48d0f60c3f36
SG master <1363028533@qq.com>
2018-02-26 19:50:54 +0800

    Update.

--- machine.h
+++ machine.h
@@ -11,28 +11,37 @@ private:
 	vector<classNode> globeClassNode;
 	vector<funcNode> globeFunc;
 
+	Memory macMem;
+
 	int func = -1;
 public:
 	Machine();
 	~Machine();
 
 	Machine *input(stateSeq *seq,
-		vector<classType> classType, vector<funcNode> func,
-		vector<varNode> var);
+		vector<classType> &classType, vector<funcNode> &func,
+		vector<varNode> &var);
 	void execute();
 	void execute(stateSeq *s, varNode *par, int funcid = -1);
 	void step(stateSeq *s);
-
 	varNode exp(varNode *e);
+
+	void print(varNode *par);
 	void out(varNode *par);
 	void outln(varNode *par);
+	varNode in(varNode *par);
+	varNode time(varNode *par);
+	varNode random(varNode *par);
+
+	void clearMem();
 
 	static void error(const char *inst, int type);
 };
 
 enum VMERROR {
 	VE_DIVBYZERO,
-	VE_TYPEMISMATCH
+	VE_TYPEMISMATCH,
+	VE_BROKEN
 };
 class MachineException {
 private:
