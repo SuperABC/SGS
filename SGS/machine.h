fddfa179056f5f668b9c728806d2ecee500216fb
SG master <1363028533@qq.com>
2018-05-24 16:36:41 +0800

    Add plugin-interface.

--- SGS/machine.h
+++ SGS/machine.h
@@ -1,1 +1,46 @@
-#pragma once
+#ifndef MACHINE_H
+#define MACHINE_H
+#include "syntax.h"
+
+class SgsMachine {
+private:
+	vector<sgs::AST *> stmts;
+
+	vector<sgs::ClassType *>classList;
+	vector<sgs::FuncProto *>funcList;
+
+	SgsMemory macMem;
+
+	void *env;
+	void initModule();
+	void loadDlls();
+
+public:
+	SgsMachine();
+	~SgsMachine();
+
+	SgsMachine *input(vector<sgs::AST *> *s,
+		vector<sgs::ClassType *> c, vector<sgs::FuncProto *> f);
+	void execute();
+	void step(sgs::AST *s);
+	void environment(void *env);
+
+	void clearMem();
+	static void error(const char *inst, int type);
+};
+
+enum SGSVMERROR {
+	VE_DIVBYZERO,
+	VE_TYPEMISMATCH,
+	VE_BROKEN
+};
+class SGSMachineException {
+private:
+	std::string msg;
+public:
+	SGSMachineException(std::string s) {
+		msg = s;
+	}
+	const char *message() { return msg.data(); }
+};
+#endif
