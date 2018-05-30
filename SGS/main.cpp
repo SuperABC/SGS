d3ddf887773f8b6ca29cbf454a3adf41324465c0
SG master <1363028533@qq.com>
2018-05-30 23:22:47 +0800

    Nothing.

--- SGS/main.cpp
+++ SGS/main.cpp
@@ -3,7 +3,7 @@
 #include <fstream>
 #include "machine.h"
 
-//#define SGS_INTERPRETER
+#define SGS_INTERPRETER
 
 using std::cin;
 using std::cout;
@@ -13,16 +13,20 @@ using namespace sgs;
 SgsLex l = SgsLex();
 SgsSyntax s = SgsSyntax();
 SgsMachine m = SgsMachine();
-
+void printBasicType(sgs::VarType *stmtVar);
+void printArrayType(sgs::VarType *stmtVar);
+void printClassType(sgs::VarType *stmtVar);
+void dealWithExpType(sgs::AST *s);
 void printBasicType(sgs::VarType *stmtVar)
 {
 	sgs::BasicType *basicVar = (sgs::BasicType *)stmtVar;
 	switch (basicVar->getBasicType())
 	{
-	case 0: std::cout << "	basicType: BT_INT" << std::endl; break;
-	case 1: std::cout << "	basicType: BT_FLOAT" << std::endl; break;
-	case 2: std::cout << "	basicType: BT_BOOL" << std::endl; break;
-	case 3: std::cout << "	basicType: BT_STRING" << std::endl; break;
+	case BASIC_TYPE::BT_INT:    std::cout << "basicType: BT_INT" << std::endl; break;
+	case BASIC_TYPE::BT_FLOAT:  std::cout << "basicType: BT_FLOAT" << std::endl; break;
+	case BASIC_TYPE::BT_BOOL:   std::cout << "basicType: BT_BOOL" << std::endl; break;
+	case BASIC_TYPE::BT_CHAR:   std::cout << "basicType: BT_CHAR" << std::endl; break;
+	case BASIC_TYPE::BT_STRING: std::cout << "basicType: BT_STRING" << std::endl; break;
 	default:
 		break;
 	}
@@ -33,14 +37,119 @@ void printArrayType(sgs::VarType *stmtVar)
 	sgs::ArrayType *arrayVar = (sgs::ArrayType *)stmtVar;
 	std::cout << "	decType: VT_ARRAY" << std::endl;
 	std::cout << "	length:  " << arrayVar->getLength() << std::endl;
+	std::cout << "	";
 	switch (arrayVar->getEleType()->getVarType())
 	{
-	case 0: printBasicType(arrayVar->getEleType()); break;
-	case 1: std::cout << "decType: VT_ARRAY" << std::endl; break;
-	case 2: std::cout << "decType: VT_CLASS" << std::endl; break;
+	case VAR_TYPE::VT_BASIC: printBasicType(arrayVar->getEleType()); break;
+	case VAR_TYPE::VT_ARRAY: printArrayType(arrayVar->getEleType()); break;
+	case VAR_TYPE::VT_CLASS: printClassType(arrayVar->getEleType()); break;
 	default:break;
 	}
 	return;
+}
+void printClassType(sgs::VarType *stmtVar)
+{
+	sgs::ClassType *classVar = (sgs::ClassType *)stmtVar;
+	std::cout << "	decType: VT_CLASS" << std::endl;
+	std::cout << "	className:  " << classVar->getName() << std::endl;
+	std::cout << "	classMember: " << std::endl;
+	int count = classVar->getEle().size();
+	vector <std::pair<VarType *, string>> classElements = classVar->getEle();
+	for (int i = 0; i < count; ++i)
+	{
+		sgs::VarType *currentMember = classElements[i].first;
+		std::cout << "	    No." << (i + 1) << " Member:" << std::endl;
+		std::cout << "	        memberName:" + classElements[i].second << std::endl;
+		std::cout << "		";
+		switch (currentMember->getVarType())
+		{
+		case VAR_TYPE::VT_BASIC: printBasicType(currentMember); break;
+		case VAR_TYPE::VT_ARRAY: printArrayType(currentMember); break;
+		case VAR_TYPE::VT_CLASS: printClassType(currentMember); break;
+		default:break;
+		}
+	}
+	return;
+}
+void printOpExp(sgs::Expression *stmtExp)
+{
+	sgs::OpExp *opExp = (sgs::OpExp *)stmtExp;
+	std::cout << "opExpOperator:" + opExp->getOp() << std::endl;
+	std::cout << "    leftExp:" << std::endl;
+	std::cout << "        ";
+	dealWithExpType(opExp->getLeft());
+	std::cout << "    RightExp:" << std::endl;
+	std::cout << "        ";
+	dealWithExpType(opExp->getRight());
+}
+void printLiteralExp(sgs::Expression *stmtExp)
+{
+	sgs::LiteralExp *literalExp = (sgs::LiteralExp *)stmtExp;
+	switch (literalExp->getType()->getVarType())
+	{
+	case VAR_TYPE::VT_BASIC:
+	{
+		sgs::BasicType *basicLiteralExp = (sgs::BasicType *)literalExp->getType();
+		switch (basicLiteralExp->getBasicType())
+		{
+		case BASIC_TYPE::BT_INT:
+		{
+			sgs::IntLiteral *intLiteralExp = (sgs::IntLiteral *)literalExp;
+			std::cout << "int value:" << intLiteralExp->getValue() << std::endl;
+			break;
+		}
+		case BASIC_TYPE::BT_FLOAT:
+		{
+			sgs::FloatLiteral *floatLiteralExp = (sgs::FloatLiteral *)literalExp;
+			std::cout << "float value:" << floatLiteralExp->getValue() << std::endl;
+			break;
+		}
+		case BASIC_TYPE::BT_BOOL:
+		{
+			sgs::BoolLiteral *boolLiteralExp = (sgs::BoolLiteral *)literalExp;
+			std::cout << "Bool value:" << boolLiteralExp->getValue() << std::endl;
+			break;
+		}
+		case BASIC_TYPE::BT_STRING:
+		{
+			sgs::StrLiteral *strLiteralExp = (sgs::StrLiteral *)literalExp;
+			std::cout << "String value:" << strLiteralExp->getValue() << std::endl;
+			break;
+		}
+		default:
+			break;
+		}
+		break;
+	}
+	case VAR_TYPE::VT_ARRAY:
+	{
+		sgs::ArrayLiteral *arrayLiteralExp = (sgs::ArrayLiteral *)literalExp;
+		std::cout << "Array value:"  << std::endl;
+		vector<Expression *>arrayConent = arrayLiteralExp->getValue();
+		int count = arrayConent.size();
+		for (int i = 0; i < count; ++i)
+		{
+			std::cout << "content " << i << std::endl;
+			dealWithExpType(arrayConent[i]);
+		}
+		break;
+	}
+	case VAR_TYPE::VT_CLASS:
+	{
+		sgs::ClassLiteral *classLiteralExp = (sgs::ClassLiteral *)literalExp;
+		std::cout << "Class value:" << std::endl;
+		vector<Expression *>classConent = classLiteralExp->getValue();
+		int count = classConent.size();
+		for (int i = 0; i < count; ++i)
+		{
+			std::cout << "content " << i << std::endl;
+			dealWithExpType(classConent[i]);
+		}
+		break;
+	}
+	default:
+		break;
+	}
 }
 enum conditionUseVarType {
 	TYPEDEF,
@@ -54,26 +163,73 @@ void dealWithVarType(sgs::AST *s, enum conditionUseVarType choice)
 	//first, do type conversion
 	switch (choice)
 	{
-	case 0: 
+	case conditionUseVarType::TYPEDEF: //TYPEDEF
 	{
 		sgs::TypeDef *currentStmt = (sgs::TypeDef *)s;
 		switch (currentStmt->getDecType()->getVarType())
 		{
-		case 0: printBasicType(currentStmt->getDecType()); break;
-		case 1: printArrayType(currentStmt->getDecType()); break;
-		case 2: std::cout << "decType: VT_CLASS" << std::endl; break;
+		case VAR_TYPE::VT_BASIC: printBasicType(currentStmt->getDecType()); break;
+		case VAR_TYPE::VT_ARRAY: printArrayType(currentStmt->getDecType()); break;
+		case VAR_TYPE::VT_CLASS: printClassType(currentStmt->getDecType()); break;
 		default:break;
 		}
 		break; 
 	}
-	case 1: {break; }
-	case 2: {break; }
+	case conditionUseVarType::CLASS:
+	{
+		sgs::ClassDef *currentStmt = (sgs::ClassDef *)s;
+		printClassType(currentStmt->getDecType());
+		break; 
+	}
+	case conditionUseVarType::EXP: 
+	{
+		sgs::Expression *currentStmt = (sgs::Expression *)s;
+		//std::cout << "	expRes:" << std::endl;
+		switch (currentStmt->getResType()->getVarType())
+		{
+		case VAR_TYPE::VT_BASIC: printBasicType(currentStmt->getResType()); break;
+		case VAR_TYPE::VT_ARRAY: printArrayType(currentStmt->getResType()); break;
+		case VAR_TYPE::VT_CLASS: printClassType(currentStmt->getResType()); break;
+		default:break;
+		}
+		break; 
+	}
 	case 3: {break; }
 	case 4: {break; }
 	default:
 		break;
 	}
 	
+}
+void dealWithExpType(sgs::AST *s)
+{
+	sgs::Expression *currentStmt = (sgs::Expression *)s;
+	switch (currentStmt->getExpType())
+	{
+	case 0: 
+		std::cout << "expType: ET_OP" << std::endl; 
+		std::cout << "    ";
+		printOpExp(currentStmt);
+		break;
+	case 1: 
+		std::cout << "expType: ET_LITERAL" << std::endl;
+		std::cout << "    ";
+		printLiteralExp(currentStmt);
+		break;
+	case 2: 
+		std::cout << "expType: ET_IDENT" << std::endl; 
+		break;
+	case 3: 
+		std::cout << "expType: ET_VISIT" << std::endl; 
+		break;
+	case 4: 
+		std::cout << "expType: ET_CALL" << std::endl; 
+		break;
+	case 5: 
+		std::cout << "expType: ET_ACCESS" << std::endl; 
+		break;
+	default:break;
+	}
 }
 void testTool(vector<sgs::AST *> stmts)
 {
@@ -97,16 +253,8 @@ void testTool(vector<sgs::AST *> stmts)
 		{
 			sgs::ClassDef *currentStmt = (sgs::ClassDef *)stmts[loopNum];
 			std::cout << "astType: AT_CLASS" << std::endl;
-			switch (currentStmt->getDecType()->getVarType())
-			{
-			case 0: std::cout << "decType: VT_BASIC" << std::endl; break;
-			case 1: std::cout << "decType: VT_ARRAY" << std::endl; break;
-			case 2: std::cout << "decType: VT_CLASS" << std::endl; break;
-			default:break;
-			}
+			dealWithVarType(currentStmt, CLASS);
 			std::cout << "className: " + currentStmt->getDecType()->getName() << std::endl;
-			std::cout << "eleList" << std::endl;
-			//std::cout << currentStmt->getDecType()->getEle() << std::endl;
 			std::cout << std::endl;
 			break;
 		}
@@ -114,23 +262,8 @@ void testTool(vector<sgs::AST *> stmts)
 		{
 			sgs::Expression *currentStmt = (sgs::Expression *)stmts[loopNum];
 			std::cout << "astType: AT_EXP" << std::endl;
-			switch (currentStmt->getExpType())
-			{
-			case 0: std::cout << "expType: ET_OP" << std::endl; break;
-			case 1: std::cout << "expType: ET_LITERAL" << std::endl; break;
-			case 2: std::cout << "expType: ET_IDENT" << std::endl; break;
-			case 3: std::cout << "expType: ET_VISIT" << std::endl; break;
-			case 4: std::cout << "expType: ET_CALL" << std::endl; break;
-			case 5: std::cout << "expType: ET_ACCESS" << std::endl; break;
-			default:break;
-			}
-			switch (currentStmt->getResType()->getVarType())
-			{
-			case 0: std::cout << "decType: VT_BASIC" << std::endl; break;
-			case 1: std::cout << "decType: VT_ARRAY" << std::endl; break;
-			case 2: std::cout << "decType: VT_CLASS" << std::endl; break;
-			default:break;
-			}
+			dealWithExpType(currentStmt);
+			dealWithVarType(currentStmt, EXP);
 			std::cout << std::endl;
 			break;
 		}
@@ -205,6 +338,7 @@ void main() {
 #else
 	testTool(s.stmts);
 #endif
-
+	//(sgs::IdExp *)(((sgs::AssignStmt*)s.stmts[5])->getLeft());
+	//dealWithExpType(((sgs::AssignStmt*)s.stmts[6])->getRight());
 	system("pause");
 }
